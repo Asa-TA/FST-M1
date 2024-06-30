d5d854c2148f0956f6926e1c786db37eb4005250
Asa-TA <athenaru@in.ibm.com>
2024-06-30 23:53:43 +0530

    Add files via upload

---
+++ SQL/Activities/SQLactivity1.sql
@@ -0,0 +1,36 @@
+REM   Script: Session 01
+REM   Activity1SQL
+
+select* from scott.emp;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+select* from scott.emp 
+-- Create salesman table 
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select* from scott.emp;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
