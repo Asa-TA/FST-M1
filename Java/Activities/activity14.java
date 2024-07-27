83379b40501db350d9e50a67112b22f5ed62ed5b
Asa-TA <athenaru@in.ibm.com>
2024-07-27 09:06:47 +0530

    Add files via upload

---
+++ Java/Activities/activity14.java
@@ -0,0 +1,45 @@
+package Activities;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
+
+public class activity14 {
+	
+    public static void main(String[] args) throws IOException {
+        try {
+            File file = new File("C:\\Users\\AsaAThenaruvi\\Desktop\\testfile.txt");
+            boolean fStatus = file.createNewFile();
+            if(fStatus) {
+                System.out.println("File created successfully!");
+            } else {
+                System.out.println("File already exists at this path.");
+            }
+ 
+            //get the file Object
+            File fileUtil = FileUtils.getFile("C:\\Users\\AsaAThenaruvi\\Desktop\\testfile.txt");
+           
+            //Read file
+            System.out.println("Data in file: " + FileUtils.readFileToString(fileUtil, "UTF8"));
+ 
+            //Create directory
+            File destDir = new File("resources");
+            //Copy file to directory
+            FileUtils.copyFileToDirectory(file, destDir);
+ 
+            //Get file from new directory
+            File newFile = FileUtils.getFile(destDir, "newfile.txt");
+            //Read data from file
+            String newFileData = FileUtils.readFileToString(newFile, "UTF8");
+            //Print it
+            System.out.println("Data in new file: " + newFileData);
+        } catch(IOException errMessage) {
+            System.out.println(errMessage);
+        }
+    }
+
+
+}
