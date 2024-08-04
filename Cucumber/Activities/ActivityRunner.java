86a2e63a7379682afad91b72904afc8c0283775e
Asa-TA <athenaru@in.ibm.com>
2024-08-04 07:22:12 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivityRunner.java
@@ -0,0 +1,14 @@
+package TestRunners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Features",
+		glue = {"StepDefinitions"},
+		tags = "ClassroomActivity1"
+)
+public class ActivityRunner {}
